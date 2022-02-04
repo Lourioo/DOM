c1ea83ec98579c66c9897156bfa6ef6e17df4296
Laura <96611296+Lourioo@users.noreply.github.com>
2022-02-04 23:18:47 +0600

    Add files via upload

---
+++ DOM/index.js
@@ -0,0 +1,14 @@
+document.querySelector("h1").innerHTML="Good bye";
+document.firstElementChild.firstElementChild.lastElementChild;
+document.firstElementChild.lastElementChild.firstElementChild.innerHTML= "Good bye";
+document.querySelector("h1").innerHTML="Today is Monday";
+document.querySelector("h1").style.color = "blue";
+var heading = document.firstElementChild.lastElementChild.firstElementChild;
+heading.innerHTML="Gulzhan";
+heading.style.color = "yellow";
+document.querySelector("li").classList.add("firstElement");
+document.querySelector("li").classList;
+document.querySelector("h1").classList.add("visibile");
+document.querySelector("li").innerHTML;
+document.querySelector("li").textContent;
+document.querySelector("a").setAttribute("href","https://facebook.com")
